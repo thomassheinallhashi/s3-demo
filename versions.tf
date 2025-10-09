519a185397c2dabbebcefbf0d8d65e3a623c8a44
thomassheinallhashi <110841436+thomassheinallhashi@users.noreply.github.com>
2025-10-09 10:43:29 -0400

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "= 3.75.2"   # prod pinned to the old line
+      version = "= 5.60"   # prod pinned to the old line
     }
   }
   required_version = ">= 1.5.0"
