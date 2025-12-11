0e6338dc93e9211d251d08ec4049fbf8e78d3cd4
thomassheinallhashi <110841436+thomassheinallhashi@users.noreply.github.com>
2025-12-11 12:59:18 -0500

    Add versioning to S3 bucket resource

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 resource "aws_s3_bucket" "demo" {
   bucket = var.bucket_name
 }
+
+resource "aws_s3_bucket_versioning" "demo" {
+  bucket = aws_s3_bucket.demo.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
