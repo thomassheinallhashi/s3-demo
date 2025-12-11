c0477f82d02b0806c843294fd795533301e1e1be
thomassheinallhashi <110841436+thomassheinallhashi@users.noreply.github.com>
2025-12-11 13:13:06 -0500

    Update AWS provider version in versions.tf
    Updated AWS provider version from 3.75.2 to 5.60.

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "= 3.75.2"  
+      version = "= 5.60"  # old version 3.75.2
     }
   }
   required_version = ">= 1.5.0"
